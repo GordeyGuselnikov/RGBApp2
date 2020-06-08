e7609aa91a2a5b5f7e2b59e50041789b025d8054
Гордей Гусельников <guselnikovgordey@icloud.com>
2020-06-08 16:07:59 +0700

    Initial Commit

---
+++ RGBApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBApp2
+//
+//  Created by Гордей Гусельников on 08.06.2020.
+//  Copyright © 2020 gordeyStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
